74848cfbd9a30689b0794da66e1abbed7b6950c0
francescafenix <francescarosa88@gmail.com>
2015-05-29 15:32:59 +0200

    Update cat2.js

--- json/cat2.js
+++ json/cat2.js
@@ -12,96 +12,96 @@ define(['i18n!nls/questions'], function(Quests) {
 						"type":"object"
 						"properties":{
 									"ask2.1.1":{
-									"title": Quests['ask2.1.1']
+									"title": Quests['ask2.1.1'],
 									"type":	"object",
 									"properties": {
 													"ask2.1.1.1":{ 
-													"title": Quests['ask2.1.1.1']
+													"title": Quests['ask2.1.1.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.1.2": {
-													"title":Quests['ask2.1.1.1.2']
+													"title":Quests['ask2.1.1.1.2'],
 													"type": "string"
 												}
 						
 										}
-				                        },
+				         },
 									"ask2.1.2":{
-									"title": Quests['ask2.1.2']
+									"title": Quests['ask2.1.2'],
 									"type":	"object",
 									"properties": {
 													"ask2.1.2.1":{ 
-													"title": Quests['ask2.1.2.1']
+													"title": Quests['ask2.1.2.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.1.2": {
-													"title":Quests['ask2.1.2.2']
+													"title":Quests['ask2.1.2.2'],
 													"type": "string"
 												}
 						
 										}
 				                        },
 									"ask2.1.3":{
-									"title": Quests['ask2.1.3']
+									"title": Quests['ask2.1.3'],
 									"type":	"object",
 									"properties": {
 													"ask2.1.3.1":{ 
-													"title": Quests['ask2.1.3.1']
+													"title": Quests['ask2.1.3.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.3.2": {
-													"title":Quests['ask2.1.3.2']
+													"title":Quests['ask2.1.3.2'],
 													"type": "string"
 												}
 						
 										}
 				                        },
 									"ask2.1.4":{
-									"title": Quests['ask2.1.4']
+									"title": Quests['ask2.1.4'],
 									"type":	"object",
 									"properties": {
 													"ask2.1.4.1":{ 
-													"title": Quests['ask2.1.4.1']
+													"title": Quests['ask2.1.4.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.4.2": {
-													"title":Quests['ask2.1.4.2']
+													"title":Quests['ask2.1.4.2'],
 													"type": "string"
 												}
 						
 										}
 				                        },
 									"ask2.1.5":{
-									"title": Quests['ask2.5.1']
+									"title": Quests['ask2.5.1'],
 									"type":	"object",
 									"properties": {
 													"ask2.1.5.1":{ 
-													"title": Quests['ask2.1.5.1']
+													"title": Quests['ask2.1.5.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.5.2": {
-													"title":Quests['ask2.1.5.2']
+													"title":Quests['ask2.1.5.2'],
 													"type": "string"
 												}
 						
 										}
 				                        },
 									"ask2.1.6":{
-									"title": Quests['ask2.1.6']
+									"title": Quests['ask2.1.6'],
 									"type":	"object",
 									"properties": {
 													"ask2.1.6.1":{ 
-													"title": Quests['ask2.1.6.1']
+													"title": Quests['ask2.1.6.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.1.2": {
-													"title":Quests['ask2.1.6.2']
+													"title":Quests['ask2.1.6.2'],
 													"type": "string"
 												}
 						
@@ -116,16 +116,16 @@ define(['i18n!nls/questions'], function(Quests) {
 						"type":"object"
 						"properties":{
 									"ask2.2.1":{
-									"title": Quests['ask2.2.1']
+									"title": Quests['ask2.2.1'],
 									"type":	"object",
 									"properties": {
 													"ask2.2.1.1":{ 
-													"title": Quests['ask2.2.1.1']
+													"title": Quests['ask2.2.1.1'],
 													"type": "string"
 													},
 													
 													"ask2.1.1.2": {
-													"title":Quests['ask2.2.1.1.2']
+													"title":Quests['ask2.2.1.1.2'],
 													"type": "string",
 													 "uniqueItems": true,
 													"format": "select",
@@ -138,16 +138,16 @@ define(['i18n!nls/questions'], function(Quests) {
 										}
 				                        },
 									"ask2.2.2":{
-									"title": Quests['ask2.2.2']
+									"title": Quests['ask2.2.2'],
 									"type":	"object",
 									"properties": {
 													"ask2.2.2.1":{ 
-													"title": Quests['ask2.2.2.1']
+													"title": Quests['ask2.2.2.1'],
 													"type": "string"
 													},
 													
 													"ask2.2.1.2": {
-													"title":Quests['ask2.2.2.2']
+													"title":Quests['ask2.2.2.2'],
 													"type": "string",
 													"uniqueItems": true,
 													"format": "select",
@@ -160,11 +160,11 @@ define(['i18n!nls/questions'], function(Quests) {
 										}
 				                        },
 									"ask2.2.3":{
-									"title": Quests['ask2.2.3']
+									"title": Quests['ask2.2.3'],
 									"type":	"object",
 									"properties": {
 													"ask2.2.3.1":{ 
-													"title": Quests['ask2.2.3.1']
+													"title": Quests['ask2.2.3.1'],
 													"type": "string",
 													"uniqueItems": true,
 													"format": "select",
@@ -175,23 +175,23 @@ define(['i18n!nls/questions'], function(Quests) {
 													},
 													
 													"ask2.2.3.2": {
-													"title":Quests['ask2.2.3.2']
+													"title":Quests['ask2.2.3.2'],
 													"type": "string"
 												}
 						
 										}
 				                        },
 									"ask2.2.4":{
-									"title": Quests['ask2.2.4']
+									"title": Quests['ask2.2.4'],
 									"type":	"object",
 									"properties": {
 													"ask2.2.4.1":{ 
-													"title": Quests['ask2.2.4.1']
+													"title": Quests['ask2.2.4.1'],
 													"type": "string"
 													},
 													
 													"ask2.2.4.2": {
-													"title":Quests['ask2.2.4.2']
+													"title":Quests['ask2.2.4.2'],
 													"type": "string",
 													"uniqueItems": true,
 													"format": "select",
@@ -204,16 +204,16 @@ define(['i18n!nls/questions'], function(Quests) {
 										}
 				                        },
 									"ask2.2.5":{
-									"title": Quests['ask2.2.1']
+									"title": Quests['ask2.2.1'],
 									"type":	"object",
 									"properties": {
 													"ask2.2.5.1":{ 
-													"title": Quests['ask2.2.5.1']
+													"title": Quests['ask2.2.5.1'],
 													"type": "string"
 													},
 													
 													"ask2.2.5.2": {
-													"title":Quests['ask2.2.5.2']
+													"title":Quests['ask2.2.5.2'],
 													"type": "string",
 													"uniqueItems": true,
 													"format": "select",
@@ -226,16 +226,16 @@ define(['i18n!nls/questions'], function(Quests) {
 										}
 				                        },
 									"ask2.2.6":{
-									"title": Quests['ask2.2.6']
+									"title": Quests['ask2.2.6'],
 									"type":	"object",
 									"properties": {
 													"ask2.2.6.1":{ 
-													"title": Quests['ask2.2.6.1']
+													"title": Quests['ask2.2.6.1'],
 													"type": "string"
 													},
 													
 													"ask2.2.1.2": {
-													"title":Quests['ask2.2.6.2']
+													"title":Quests['ask2.2.6.2'],
 													"type": "string",
 													"uniqueItems": true,
 													"format": "select",
