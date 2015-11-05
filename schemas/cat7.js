32ebf952fc6fdccd4280be9a8cfa3ea789c41b19
stefanocudini <stefano.cudini@gmail.com>
2015-11-05 19:44:26 +0100

    cat 7

--- schemas/cat7.js
+++ schemas/cat7.js
@@ -5,81 +5,52 @@ define(['i18n!nls/questions'], function(Quests) {
 		"description":Quests["ask17_0"],
 	    "properties": {
 	        "ask17a": {
-				"title":Quests["ask17a"],
-	            "oneOf":[
-					{"title":Quests["no"],"type":"null"},
-					{"title":Quests["yes"],"type":"object","properties":{
-
-						"ask17_0a": {
-							"title": Quests['ask17_0a'],
-							"type": "array",
-							"minItems": 1,
-							"maxItems": 5,
-							"items": {
-								"type": "string",
-								"title": " ",
-								"default": ""
-							},
-							"additionalItems":false
-						}
-
-
-					}}
-
+				"title": Quests["ask17a"],
+	            "oneOf": [
+					{
+						"title": Quests["no"],
+						"type":"null"
+					},
+					{
+						"title": Quests["yes"],
+						"description": Quests['ask17_0a'],
+						"type": "array",
+						"$ref": "#/definitions/items5"
+					}
 				]
 	        },
 
 			"ask17b": {
-				"title":Quests["ask17b"],
-				"oneOf":[
-					{"title":Quests["no"],"type":"null"},
-					{"title":Quests["yes"],"type":"object","properties":{
-
-						"ask17_0b": {
-							"title": Quests['ask17_0b'],
-							"type": "array",
-							"minItems": 1,
-							"maxItems": 5,
-							"items": {
-								"type": "string",
-								"title": " ",
-								"default": ""
-							},
-							"additionalItems":false
-						}
-
-
-					}}
-
+				"title": Quests["ask17b"],
+				"oneOf": [
+					{
+						"title": Quests["no"],
+						"type": "null"
+					},
+					{
+						"title": Quests["yes"],
+						"description": Quests['ask17_0b'],
+						"type": "array",
+						"$ref": "#/definitions/items5"
+					}
 				]
 			},
 
 			"ask18": {
 				"title": Quests['ask18'],
-"oneOf":[
-	{"title":Quests["no"],"type":"null"},
-	{"title":Quests["yes"],"type":"object","properties":{
-
-		"ask18_1": {
-			"title": Quests['ask18_1'],
-			"type": "array",
-			"minItems": 1,
-			"maxItems": 5,
-			"items": {
-				"type": "string",
-				"title": " ",
-				"default": ""
-			},
-			"additionalItems":false
-		}
-
-
-	}}
-
-]
+				"oneOf": [
+					{
+						"title": Quests["no"],
+						"type":"null"
+					},
+					{
+						"title": Quests["yes"],
+						"description": Quests['ask18_1'],
+						"type": "array",
+						"$ref": "#/definitions/items5"
+					}
+				]
 			}
-
-
 	    }
 	};
 });
