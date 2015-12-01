9ff4eee94803f5d0ebf58864fa4a274324b60a80
stefanocudini <stefano.cudini@gmail.com>
2015-12-01 16:56:06 +0100

    fix cat2 title

--- schemas/cat3.js
+++ schemas/cat3.js
@@ -1,7 +1,7 @@
 define(['i18n!nls/questions'], function(Quests) {
 	return {
 		"type": "object",
-		"title": Quests['cat1'],
+		"title": Quests['cat3'],
 		"description": Quests['ask3_0'],
 		"properties": {
 			"ask3": {
